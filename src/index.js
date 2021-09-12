0d0f6d879dbd16abf723d574e64c05e5fd0e96f9
Helgard Richard Ferreira <hrfer3@gmail.com>
2021-09-12 18:00:44 +0200

    feat: Complete "Asset Management"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import _ from "lodash";
+import join from "lodash/join";
+import "./style.css";
+// import SynthWave from "./synthWave.png";
+import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div
+  /* const myImage = new Image();
+  myImage.src = SynthWave;
+
+  element.appendChild(myImage); */
+
+  console.log(Notes);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,33 @@ module.exports = {
   entry: "./src/index.js",
   mode: "production",
   output: {
-    publicPath: "/",
+    publicPath: "./",
     path: path.resolve(__dirname, "dist"),
     // filename: "[name].[contenthash].bundle.js",
-    filename: "main.js",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+    ],
   },
 };
