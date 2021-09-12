126f3163f8ece5276ae5290c3f04a75b0c64182e
Helgard Richard Ferreira <hrfer3@gmail.com>
2021-09-12 18:09:02 +0200

    wip: Begin "Output Management"

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import join from "lodash/join";
-import "./style.css";
-// import SynthWave from "./synthWave.png";
-import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // Add the image to our existing div
-  /* const myImage = new Image();
-  myImage.src = SynthWave;
-
-  element.appendChild(myImage); */
-
-  console.log(Notes);
 
   return element;
 }
